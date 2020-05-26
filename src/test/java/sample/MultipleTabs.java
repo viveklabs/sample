403e607616af58bfbc517568c83d4c3cf835aec7
viveklabs <vivek.tcs.certifications@gmail.com>
2020-05-26 22:21:31 +0530

    tab handling

---
+++ src/test/java/sample/MultipleTabs.java
@@ -0,0 +1,56 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MultipleTabs {
+
+	static ArrayList<String> windowMap = new ArrayList<String>();
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.qafox.com/selenium/selenium-practice/");
+
+		String parentWindow = driver.getWindowHandle();
+
+		List<WebElement> allLinks = driver.findElements(By.xpath("//a[contains(text(),'Click here')]"));
+
+		for (WebElement e : allLinks) {
+			e.click();
+		}
+		driver.switchTo().window(parentWindow);
+
+		Set<String> allWindows = driver.getWindowHandles();
+
+		for (String child : allWindows) {
+
+			if (!child.equalsIgnoreCase(parentWindow)) {
+
+				windowMap.add(child);
+			}
+		}
+
+		/*
+		 * for (int i = windowMap.size() - 1; i >= 0; i--) {
+		 * driver.switchTo().window(windowMap.get(i)); driver.close(); }
+		 */
+		
+		 for (int i = 0; i<windowMap.size(); i++)  // to close the window in descending order
+		 {
+		 driver.switchTo().window(windowMap.get(i)); driver.close();
+		 }
+	}
+}
